e0b5b4c5e275185c20caa40dddf038f613439899
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-22 20:25:35 -0300

    feat(data): initial Data fuctions

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,29 @@
-use crate::flatlander::Flatlander;
+use crate::{flatlander::Flatlander, shadow_error::ShadowError};
 
 #[derive(Debug)]
 pub struct Data {
-    pub degrees: i32,
-    pub flatlanders: Vec<Flatlander>,
+    degrees: i32,
+    flatlanders: Vec<Flatlander>,
+}
+
+impl Data {
+    pub fn new(degrees: i32, flatlanders: Vec<Flatlander>) -> Self {
+        !unimplemented!()
+    }
+
+    pub fn set_degrees(&mut self, degrees: i32) -> Result<(), ShadowError> {
+        !unimplemented!()
+    }
+
+    pub fn set_flatlander(&mut self, x: i32, h: i32) -> Result<(), ShadowError> {
+        !unimplemented!()
+    }
+
+    pub fn sort(&mut self) -> Result<(), ShadowError> {
+        !unimplemented!()
+    }
+
+    pub fn calculate_total_shadow_length(&self) -> Result<f64, ShadowError> {
+        !unimplemented!()
+    }
 }
