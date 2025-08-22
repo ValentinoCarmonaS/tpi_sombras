461c803aee3cadc72f43e3990f5525316ccac32e
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-22 17:26:19 -0300

    feat(main): create Main logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod shadow_error;
+mod read_input;
+mod run;
+mod data;
+mod flatlander;
+
+use run::run;
+
 fn main() {
-    println!("Hello, world!");
+    match run() {
+        Ok(ans) => println!("{}", ans),
+        Err(e) => e.show_error(),
+    }
 }
