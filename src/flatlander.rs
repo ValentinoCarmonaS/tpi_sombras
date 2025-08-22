92cc3c59d771147bc0acaa52d5eb914b15ed2a32
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-22 20:43:37 -0300

    feat(Flatlander):  implement new

--- src/flatlander.rs
+++ src/flatlander.rs
@@ -1,5 +1,19 @@
+use crate::shadow_error::ShadowError;
+
 #[derive(Debug)]
 pub struct Flatlander {
     pub x: i32,
     pub h: i32,
+}
+
+impl Flatlander {
+    pub fn new(x: i32, h: i32) -> Result<Self, ShadowError> {
+        if x < 0 || x > 310000 {
+            return Err(ShadowError::InvalidPositionOrHeightError { value: x });
+        } else if h < 1 || h > 1000 {
+            return Err(ShadowError::InvalidPositionOrHeightError { value: h });
+        }
+        
+        Ok(Self { x, h })
+    }
 }
