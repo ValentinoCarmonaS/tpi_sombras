6410eb7a1c7f2ab93186aefb79dfa679f73b42c9
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-22 17:28:24 -0300

    feat(shadow_errors): create shadow_errors enum (first approach)

---
+++ src/shadow_error.rs
@@ -0,0 +1,9 @@
+#[derive(Debug)]
+pub enum ShadowError {
+    // no hay dos datos al principio !!!
+    // error en el angulo !!!
+
+    // error en la lectura de stdin
+    // la linea actual tiene >< a 2 elementos
+    // no hay la cantidad de flatlanders que se indican
+}
