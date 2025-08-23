472fe4df2a6940f1a89ab365ffeec0101e7c4414
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-22 22:47:41 -0300

    feat(data): implement set_degrees, sort and new

--- src/data.rs
+++ src/data.rs
@@ -2,37 +2,41 @@ use crate::{flatlander::Flatlander, shadow_error::ShadowError};
 
 #[derive(Debug)]
 pub struct Data {
-    degrees: i32,
+    theta: i32,
     flatlanders: Vec<Flatlander>,
 }
 
 impl Data {
-    pub fn new(degrees: i32, flatlanders: Vec<Flatlander>) -> Self {
-        !unimplemented!()
+    pub fn new() -> Self {
+        Self {
+            theta: 0,
+            flatlanders: Vec::new(),
+        }
     }
 
-    pub fn set_degrees(&mut self, degrees: i32) -> Result<(), ShadowError> {
-        !unimplemented!()
+    pub fn set_degrees(&mut self, theta: i32) -> Result<(), ShadowError> {
+        if theta < 10 || theta > 80 {
+            return Err(ShadowError::InvalidAngleError { value: theta });
+        }
+
+        self.theta = theta;
+        Ok(())
     }
 
     pub fn set_flatlander(&mut self, x: i32, h: i32) -> Result<(), ShadowError> {
-        if x < 0 || x > 310000 {
-            return Err(ShadowError::InvalidPositionOrHeightError { value: x });
-        } else if h < 1 || h > 1000 {
-            return Err(ShadowError::InvalidPositionOrHeightError { value: h });
-        }
-
-        let flatlander = Flatlander::new(x, h);
+        let flatlander = Flatlander::new(x, h)?;
         self.flatlanders.push(flatlander);
 
         Ok(())
     }
 
-    pub fn sort(&mut self) -> Result<(), ShadowError> {
-        !unimplemented!()
+    pub fn sort(&mut self) {
+        // Sort the flatlander by x
+        self.flatlanders
+            .sort_by_key(|flatlander| flatlander.get_x());
     }
 
-    pub fn calculate_total_shadow_length(&self) -> Result<f64, ShadowError> {
+    pub fn calculate_total_shadow_length(self) -> Result<f64, ShadowError> {
         !unimplemented!()
     }
 }
