00918183ebe9237871c20f9dcd1a3c126f6e91c2
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-23 14:59:01 -0300

    feat(data): implement calculate_total_shadow_length and UTest

--- src/data.rs
+++ src/data.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 use crate::{flatlander::Flatlander, shadow_error::ShadowError};
 
 #[derive(Debug)]
@@ -36,7 +38,182 @@ impl Data {
             .sort_by_key(|flatlander| flatlander.get_x());
     }
 
-    pub fn calculate_total_shadow_length(self) -> Result<f64, ShadowError> {
-        !unimplemented!()
+    pub fn calculate_total_shadow_length(self) -> f64 {
+        let theta_rad = (self.theta as f64) * PI / 180.0;
+        let tan = theta_rad.tan();
+
+        let mut ans = 0.0;
+        let mut current_start = -1.0;
+        let mut current_end = -1.0;
+        
+        for flatlander in self.flatlanders {
+            let x = flatlander.get_x() as f64;
+            let l = x + flatlander.calculate_shadow_length(tan);
+
+            if current_start <= x && current_end >= x {
+                current_end = current_end.max(l);
+            }
+
+            if x > current_end && current_start <= current_end {
+                ans += current_end - current_start;
+                current_start = x;
+                current_end = l;
+            }
+        }
+
+        ans +current_end - current_start
     }
+
+    #[allow(dead_code)]
+    pub fn get_theta(&self) -> i32 {
+        self.theta
+    }
+
+    #[allow(dead_code)]
+    pub fn get_flatlanders(&self) -> &Vec<Flatlander> {
+        &self.flatlanders
+    }
+}
+
+#[test]
+fn test_set_degrees() {
+    let mut data = Data::new();
+    let theta = 45;
+
+    match data.set_degrees(theta) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set degrees"),
+    }
+
+    assert_eq!(
+        data.get_theta(),
+        theta,
+        "Error in set degrees assert equals"
+    );
+}
+
+#[test]
+fn test_set_degrees_error() {
+    let mut data = Data::new();
+    let theta = 0;
+    match data.set_degrees(theta) {
+        Ok(_) => assert!(false, "Error in set degrees"),
+        Err(_) => assert!(true),
+    }
+}
+
+#[test]
+fn test_set_flatlander() {
+    let mut data = Data::new();
+    let x = 0;
+    let h = 10;
+
+    match data.set_flatlander(x, h) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+
+    assert_eq!(
+        data.flatlanders[0].get_x(),
+        x,
+        "Error in set flatlander assert equals x"
+    );
+    assert_eq!(
+        data.flatlanders[0].get_h(),
+        h,
+        "Error in set flatlander assert equals h"
+    );
+}
+
+#[test]
+fn test01_calculate_total_shadow_length() {
+    let mut data = Data::new();
+    match data.set_degrees(45) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set degrees"),
+    }
+    match data.set_flatlander(0, 10) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+    match data.set_flatlander(5, 10) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+
+    let epsilon = 1e-4;
+    let expected = 15.0000000000000;
+    let actual = data.calculate_total_shadow_length();
+
+    assert!(
+        (expected - actual).abs() < epsilon,
+        "Error in calculate total shadow length, expected: {}, got: {}",
+        expected,
+        actual
+    );
+}
+
+#[test]
+fn test02_calculate_total_shadow_length() {
+    let mut data = Data::new();
+    match data.set_degrees(30) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set degrees"),
+    }
+    match data.set_flatlander(0, 100) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+    match data.set_flatlander(50, 150) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+    
+    match data.set_flatlander(100, 200) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+
+    let epsilon = 1e-4;
+    let expected = 446.4101615137755;
+    let actual = data.calculate_total_shadow_length();
+
+    assert!(
+        (expected - actual).abs() < epsilon,
+        "Error in calculate total shadow length, expected: {}, got: {}",
+        expected,
+        actual
+    );
+}
+
+#[test]
+fn test03_calculate_total_shadow_length() {
+    let mut data = Data::new();
+    match data.set_degrees(45) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set degrees"),
+    }
+    match data.set_flatlander(0, 100) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+    match data.set_flatlander(50, 150) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+    match data.set_flatlander(100, 200) {
+        Ok(_) => (),
+        Err(_) => return assert!(false, "Error in set flatlander"),
+    }
+
+    let epsilon = 1e-4;
+    let expected = 300.00000000000006;
+    let actual = data.calculate_total_shadow_length();
+
+    assert!(
+        (expected - actual).abs() < epsilon,
+        "Error in calculate total shadow length, expected: {}, got: {}",
+        expected,
+        actual
+    );
 }
