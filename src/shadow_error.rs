3a307a80f6e77321ad93c1f9d0b205f2db1b8f28
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-22 22:50:53 -0300

    feat(ShadowError): add the errors InvalidNumFlatlandersError, InvalidLenLineError, ParseIntError and InvalidAngleError

--- src/shadow_error.rs
+++ src/shadow_error.rs
@@ -1,19 +1,39 @@
 #[derive(Debug)]
 pub enum ShadowError {
-    // no hay dos datos al principio !!!
-    // error en el angulo !!!
+    // el angulo es incorrecto !!!
+    InvalidAngleError { value: i32 },
 
     // error en la lectura de stdin
+    ParseIntError,
+
     // la linea actual tiene >< a 2 elementos
+    InvalidLenLineError,
+
     // no hay la cantidad de flatlanders que se indican
+    InvalidNumFlatlandersError { value_expected: i32, value_got: i32 },
+
+    // Error en flatlander
     InvalidPositionOrHeightError { value: i32 },
+
 }
 
 impl ShadowError {
-    fn fmt(&self) {
+    pub fn show_error(&self) {
         match self {
             Self::InvalidPositionOrHeightError { value } => {
                 eprintln!("Invalid position or height: {}", value)
+            }
+            Self::InvalidAngleError { value } => {
+                eprintln!("Invalid angle: {}", value)
+            }
+            Self::InvalidLenLineError => {
+                eprintln!("Invalid length line, the lenght is not 2")
+            }
+            Self::ParseIntError => {
+                eprintln!("Error in parse int")
+            }
+            Self::InvalidNumFlatlandersError { value_expected, value_got } => {
+                eprintln!("Invalid number of flatlanders, expected {} but got: {}", value_expected, value_got)
             }
         }
     }
