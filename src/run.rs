0ff03f3830f29d88009e4e52c3314f7c4612be35
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-22 22:49:14 -0300

    refact(run): update run

--- src/run.rs
+++ src/run.rs
@@ -4,13 +4,13 @@ use crate::read_input::read_input;
 
 pub fn run() -> Result<f64, ShadowError> {
     // Read the input from stdin
-    let input_list = read_input()?;
+    let mut input_list = read_input()?;
 
     // Sort the flatlanders by x
-    input_list.sort()?;
+    input_list.sort();
 
     // Calculate the total shadow length;
     let ans = input_list.calculate_total_shadow_length()?;
 
-    Ok(ans);
+    Ok(ans)
 }
