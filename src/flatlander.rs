a136d57fea08a9032782d94e2abbd3942921cf22
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-23 14:58:38 -0300

    feat(flatlanders): implement calculate_shadow_length and UTest

--- src/flatlander.rs
+++ src/flatlander.rs
@@ -1,5 +1,4 @@
-use std::cmp::Ordering;
-use crate::shadow_error::ShadowError;
+use crate::{shadow_error::ShadowError};
 
 #[derive(Debug)]
 pub struct Flatlander {
@@ -18,7 +17,85 @@ impl Flatlander {
         Ok(Self { x, h })
     }
 
+    pub fn calculate_shadow_length(&self, tan: f64) -> f64 {
+        (self.h as f64) / tan
+    }
+
+    #[allow(dead_code)]
     pub fn get_x(&self) -> i32 {
         self.x
+    }
+
+    #[allow(dead_code)]
+    pub fn get_h(&self) -> i32 {
+        self.h
+    }
+}
+
+#[test]
+fn test_new() {
+    let x = 0;
+    let h = 10;
+
+    match Flatlander::new(x, h) {
+        Ok(flatlander) => {
+            assert_eq!(
+                flatlander.get_x(),
+                x,
+                "Error in new flatlander assert equals x"
+            );
+            assert_eq!(
+                flatlander.get_h(),
+                h,
+                "Error in new flatlander assert equals h"
+            )
+        }
+        Err(_) => return assert!(false, "Error in new flatlander"),
+    }
+}
+
+#[test]
+fn test_calculate_shadow_length() {
+    let x = 0;
+    let h = 10;
+
+    match Flatlander::new(x, h) {
+        Ok(flatlander) => {
+            let epsilon = 1e-4;
+
+            // Angulo de 45
+            let tan1 = 1.0;
+            let expected1 = 10.0;
+            let actual1 = flatlander.calculate_shadow_length(tan1);
+            assert!(
+                (expected1 - actual1).abs() < epsilon,
+                "Error in calculate shadow length expected {}, got {}",
+                expected1,
+                actual1
+            );
+
+            // Angulo de 30
+            let tan2 = 0.57735;
+            let expected2 = 17.32050;
+            let actual2 = flatlander.calculate_shadow_length(tan2);
+            assert!(
+                (expected2 - actual2).abs() < epsilon,
+                "Error in calculate shadow length expected {}, got {}",
+                expected2,
+                actual2
+            );
+
+            // Angulo de 70
+            let tan3 = 2.74747;
+            let expected3 = 3.63970;
+            let actual3 = flatlander.calculate_shadow_length(tan3);
+            assert!(
+                (expected3 - actual3).abs() < epsilon,
+                "Error in calculate shadow length expected {}, got {}",
+                expected3,
+                actual3
+            )
+        }
+        Err(_) => return assert!(false, "Error in new flatlander"),
     }
 }
